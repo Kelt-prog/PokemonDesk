daf2fcd0c6d46fbf42e7180c303b3500acc21567
Dmity <kelt23@gmail.com>
2020-11-03 21:40:38 +0200

    Add ESlint && Prettier

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import cn from 'classnames';
+
+import s from './App.module.scss';
+
+const App = () => {
+  return <div className={cn(s.header, 'color')}>YES WE1 DID IT! This is App Components</div>;
+};
+
+export default App;
